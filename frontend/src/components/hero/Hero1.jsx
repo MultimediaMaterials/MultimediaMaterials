7af90e0ad1650c9bb733e4a5a216690ec7008190
sk5s <samko5sam@tutanota.com>
2025-10-13 11:32:50 +0800

    Add basic .gitignore and pyrightconfig, update course content, update frontend dependencies, and improve Hero1 component styling

--- frontend/src/components/hero/Hero1.jsx
+++ frontend/src/components/hero/Hero1.jsx
@@ -1,13 +1,21 @@
-const Hero1 = ({title="Hello there", description="Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.", btnText, btnAction, height=600}) => {
+const Hero1 = ({
+  title = "Hello there",
+  description = "Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.",
+  btnText,
+  btnAction,
+  height = 600,
+}) => {
   return (
-    <div className={`hero bg-base-200 h-[${height}px]`}>
+    <div className={`hero bg-base-200`} style={{ height }}>
       <div className="hero-content text-center">
         <div className="max-w-xl">
           <h1 className="text-6xl font-bold">{title}</h1>
-          <div className="py-6 text-2xl">
-            {description}
-          </div>
-          {btnText && (<button onClick={btnAction} className="btn btn-primary btn-xl">{btnText}</button>)}
+          <div className="py-6 text-2xl">{description}</div>
+          {btnText && (
+            <button onClick={btnAction} className="btn btn-primary btn-xl">
+              {btnText}
+            </button>
+          )}
         </div>
       </div>
     </div>
